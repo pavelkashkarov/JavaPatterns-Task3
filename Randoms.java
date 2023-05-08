99b824223a99f6ee3c9205ddc24abb977cae3553
Kashkarov Pavel <72447188+pavelkashkarov@users.noreply.github.com>
2023-05-08 21:05:52 +0300

    add new class

---
+++ Randoms.java
@@ -0,0 +1,31 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+
+    private final Random random;
+
+    private final int min;
+    private final int max;
+
+    public Randoms(int min, int max) {
+        random = new Random();
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return min +  random.nextInt(max - min + 1);
+            }
+        };
+    }
+}
